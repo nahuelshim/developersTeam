7cbb3d8b6d6dc0b1b52f7fcce94ba00fe0366295
Jorarm <christian.galvez.hurtado@hotmail.com>
2023-05-24 12:31:49 +0200

    add servicies

--- src/Domain/Flower.java
+++ src/Domain/Flower.java
@@ -32,6 +32,6 @@ public class Flower extends  Product {
 
     @Override
     public String toString() {
-        return super.toString() + this.color;
+        return super.toString() + color;
     }
 }
